7e4fdfbb7205c9f1fbe244071382eb19650586ba
tcarey97 <125526091+tcarey97@users.noreply.github.com>
2023-11-17 15:50:08 +1100

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -44,16 +44,19 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  console.log("hello");
-  let isbn = req.params.isbn;
-  let review = req.query.review;
-  let currentReviewer = req.session.authorization['username'];
-  let foundReview = Object.values(books)[isbn].review;
-
-  foundReview[currentReviewer] = review;
-  console.log(foundReview);
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn];
+    if (filtered_book) {
+        let review = req.query.review;
+        let reviewer = req.session.authorization['username'];
+        if(review) {
+          filtered_book['reviews'][reviewer] = review;
+          books[isbn] = filtered_book;
+      }
+      res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
+  } else{
+        res.send("Unable to find this ISBN!");
+  }
 });
 
 module.exports.authenticated = regd_users;
